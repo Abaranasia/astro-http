d115113fad88c1f0480a89a6a4a9f7f9f3cbde18
Abaranasia <rankirlian@gmail.com>
2024-11-10 14:22:51 +0100

    Insert Clients table rows

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,13 @@
-import { db } from 'astro:db';
+import { Clients, db } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	await db.insert(Clients).values([
+		{id: 1, name: 'Rolo', age: 38, isActive: true},
+		{id: 2, name: 'Ann', age: 23, isActive: true},
+		{id: 3, name: 'Sergei', age: 43, isActive: true},
+		{id: 4, name: 'Alphonse', age: 31, isActive: true},
+		{id: 5, name: 'Katherine', age: 28, isActive: false},
+	])
+
 }
