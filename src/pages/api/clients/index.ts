2fd7887042f9d8fe6028bb770e712a77af8a0e83
Abaranasia <rankirlian@gmail.com>
2024-11-10 18:03:07 +0100

    Improve post and getAll endpoints

--- src/pages/api/clients/index.ts
+++ src/pages/api/clients/index.ts
@@ -1,4 +1,5 @@
-import type { APIRoute  } from "astro";
+import type { APIRoute } from "astro";
+import { Clients, db } from "astro:db";
 /**
  * This file collects the endpoint for all clients (get) or for posting new clientes (not having ClientId)
  */
@@ -6,41 +7,64 @@ import type { APIRoute  } from "astro";
 export const prerender = false; // Make this page server side rendered (not static)
 
 export const GET: APIRoute = async ({ params, request }) => {
-    const {id} = params;
-
-    // const post = await getCollection('Clients');
-
- /*    if (!post) {
-        return new Response(JSON.stringify({ msg: `Post ${id} not found`}), {
-            status: 404,
+    try {
+        const users = await db.select().from(Clients);
+        return new Response(JSON.stringify(users), {
+            status: 200,
             headers: {
               "Content-Type": "Application/json",
             },
-        });
-    } */
-const body = {
-    method: 'getAll general',
-}
-    return new Response(JSON.stringify(body), {
-        status: 200,
-        headers: {
-            "Content-Type": "Application/json",
-          },
-    })
+          });
+        
+    } catch (error) {
+        return new Response(
+            JSON.stringify({
+              msg: "no users were found!",
+            }),
+            {
+              status: 401,
+              headers: {
+                "Content-Type": "Application/json",
+              },
+            }
+          );        
+    }
 };
 
 export const POST: APIRoute = async ({ params, request }) => {
+  try {
+    const { id, ...body } = await request.json(); // by separating the id, we can ignore incoming ids and create ours
 
-    const body = await request.json();
+    const resp = await db.insert(Clients).values({
+        ...body
+    });
 
-    return new Response(JSON.stringify({
-        method: 'post',
+    console.log('resp :>> ', resp);
+
+
+    return new Response(
+      JSON.stringify({
+        method: "post",
         ...body,
-    }), 
-    {
+      }),
+      {
         status: 201,
         headers: {
+          "Content-Type": "Application/json",
+        },
+      }
+    );
+  } catch (error) {
+    return new Response(
+        JSON.stringify({
+          msg: "no body found",
+        }),
+        {
+          status: 401,
+          headers: {
             "Content-Type": "Application/json",
           },
-    })
+        }
+      );
+  }
 };
