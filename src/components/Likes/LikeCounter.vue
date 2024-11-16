63f1a56f341f095430596e6e2a431fe82304007d
Abaranasia <rankirlian@gmail.com>
2024-11-16 22:12:58 +0100

    Add Vue component with basic functionality (no debounce)

---
+++ src/components/Likes/LikeCounter.vue
@@ -0,0 +1,61 @@
+<template>
+    <div v-if="isLoading">
+        Loading ...
+    </div>
+
+    <button v-else-if="likeCount===0" @click="handleLike">
+        Like this post
+    </button>
+    
+    <button v-else @click="handleLike">
+        Like!
+        <span>{{ likeCount }}</span>
+    </button>
+</template>
+
+<script lang="ts" setup>
+    import { ref } from 'vue';
+
+    interface Props {
+        postId: string;
+    }
+
+    const props = defineProps<Props>();
+
+    const likeCount= ref(0);
+    const likeClicks= ref(0);
+    const isLoading= ref(true);
+
+
+    const handleLike = () => {
+        likeCount.value= likeCount.value + 1;
+    }
+
+    const getCurrentLikes = async() => {
+        const resp = await fetch(`/api/posts/likes/${props.postId}`)
+        if ( !resp.ok) return;
+
+        const data = await resp.json();
+        likeCount.value= data.likes;
+        isLoading.value= false;
+    }
+
+    getCurrentLikes(); // Executes once when component is loaded
+
+</script>
+
+<style scoped>
+button {
+    background-color: #5e51bc;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: all 0.3s;
+}
+
+button:hover {
+    background-color: #4a3f9a;
+}
+</style>
