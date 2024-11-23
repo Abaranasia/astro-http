73639179a3bb37d8137387ca76195af583432d64
Abaranasia <rankirlian@gmail.com>
2024-11-23 14:04:26 +0100

    Add alternative component to consume the server action for get likes

---
+++ src/components/Likes/LikeCounterAction.vue
@@ -0,0 +1,99 @@
+<template>
+    <div v-if="isLoading">
+        Loading ...
+    </div>
+
+    <button v-else-if="likeCount===0" @click="handleLike">
+        Like this post
+    </button>
+    
+    <button v-else @click="handleLike">
+        Like!
+        <span>{{ likeCount }}</span>
+        {{ likeClicks }}
+    </button>
+</template>
+
+<script lang="ts" setup>
+    import { ref, watch } from 'vue';
+    import confetti from 'canvas-confetti';
+    import debounce from 'lodash.debounce';
+    import { actions } from 'astro:actions';
+
+    interface Props {
+        postId: string;
+    }
+
+    const props = defineProps<Props>();
+
+    const likeCount= ref(0);
+    const likeClicks= ref(0);
+    const isLoading= ref(true);
+
+    watch(likeCount, debounce(() => {
+        console.log('likeCount :>> ', likeCount.value);
+
+        fetch(`/api/posts/likes/${props.postId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({likes: likeClicks.value})
+        });
+        likeClicks.value = 0;
+    }, 500));
+
+    const handleLike = async () => {
+        likeCount.value= likeCount.value + 1;
+        likeClicks.value++;
+        
+        const resp = await actions.getGreeting({ // Server action examnple
+            name: 'pepe',
+            age: '25',
+            isActive: true,
+        });
+        
+        if (resp.data) console.log('resp.data :>> ', resp.data);
+
+        confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: {
+                x: Math.random(),
+                y: Math.random() - 0.2,
+            }
+        })
+    }
+
+    const getCurrentLikes = async() => {
+/*      const resp = await fetch(`/api/posts/likes/${props.postId}`)
+        if ( !resp.ok) return; 
+        const data = await resp.json();
+        */
+
+        const { data, error } = await actions.getPostLikes(props.postId);
+        if (error) return alert(error);
+        
+        likeCount.value= data.likes;
+        isLoading.value= false;
+    }
+
+    getCurrentLikes(); // Executes once when component is loaded
+
+</script>
+
+<style scoped>
+button {
+    background-color: #5e51bc;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: all 0.3s;
+}
+
+button:hover {
+    background-color: #4a3f9a;
+}
+</style>
